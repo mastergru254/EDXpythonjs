86f73108d277d43cbd0a93bab1446d1905a4ce98
Evans korir <evanrotich12@gmail.com>
2022-12-08 08:05:35 -0800

    conditional statiment if

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello dunia!!")
+name = input("Name:")
+print("Hello " + name)
+
+n = int(input("enter your number: "))
+if n > 0:
+    print("your number is positive")
+elif n < 0:
+    print("negative number")
+else:
+    print("number is zero")
+
