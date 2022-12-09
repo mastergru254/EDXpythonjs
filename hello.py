d2694b71a90f82bde6e72845c0ff361d8c7518b5
Evans korir <evanrotich12@gmail.com>
2022-12-09 10:22:21 -0800

    sequeance

--- hello.py
+++ hello.py
@@ -10,3 +10,16 @@ elif n < 0:
 else:
     print("number is zero")
 
+names = ["Evans","Melvin","Moses"]
+
+names.append("Slyvia")
+names.sort
+print(names)
+#create empty set
+s = set()
+#add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+print(s)
+print(f" the set has ", len(s) ,"elements")
