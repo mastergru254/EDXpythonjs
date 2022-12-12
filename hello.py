87c892fe5e9e13cad37c0e08ab38e8f9c0fa295b
Evans korir <evanrotich12@gmail.com>
2022-12-11 16:51:57 -0800

    oop added

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ print("Hello dunia!!")
 name = input("Name:")
 print("Hello " + name)
 
+def square(x):
+    return x * x
 n = int(input("enter your number: "))
 if n > 0:
     print("your number is positive")
@@ -9,7 +11,7 @@ elif n < 0:
     print("negative number")
 else:
     print("number is zero")
-
+# adding a list
 names = ["Evans","Melvin","Moses"]
 
 names.append("Slyvia")
@@ -23,3 +25,9 @@ s.add(2)
 s.add(3)
 print(s)
 print(f" the set has ", len(s) ,"elements")
+# adding a loop
+for i in range(10):
+    print(i)
+#loop over the names list
+for name in names:
+    print(name)
